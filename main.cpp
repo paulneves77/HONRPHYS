ce22f1199d070fdc74d36ce2ac09d87085a9face
Paul <paulneves77@gmail.com>
2015-09-30 21:59:15 -0400

    first commit

---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+ 
+using namespace std;
+int main() {
+  cout <<"Hello World!" << endl;    //Print hello world to screen followed by end line (endl)
+  return 0;    //Exit the program
+}
